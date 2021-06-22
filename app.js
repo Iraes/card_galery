72ccd491aef3a8a7f1c38df91f5347c4668c9ff9
Олег Коробов <korobov@nevis.spb.ru>
2021-06-22 10:56:25 +0300

    Добавлена функция выбора активного слайда

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const slides = document.querySelectorAll(".slide");
+    function slidesPlugin(activeSlide = 1) {
+        const slides = document.querySelectorAll(".slide");
 
-    for (const slide of slides) {
-        slide.addEventListener('click', () => {
-            clearActiveClasses();
-            slide.classList.add('active');
-        })
+        slides[activeSlide].classList.add("active")
+        for (const slide of slides) {
+            slide.addEventListener('click', () => {
+                clearActiveClasses();
+                slide.classList.add('active');
+            })
+        }
+
+        function clearActiveClasses() {
+            slides.forEach(slide => {
+                slide.classList.remove('active');
+            })
+        }
     }
 
-    function clearActiveClasses() {
-        slides.forEach(slide => {
-            slide.classList.remove('active');
-        })
-    }
+    slidesPlugin()
 })
